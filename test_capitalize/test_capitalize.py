7c0cc066854fd08fa602eadb41e34afd07e830fe
AntiViruS90 <slava90nikitin90@gmail.com>
2024-07-16 20:42:16 +0300

    first commit with new files of repo

---
+++ test_capitalize/test_capitalize.py
@@ -0,0 +1,9 @@
+from capitalize import capitalize
+
+if capitalize.capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неверно!')
+
+if capitalize.capitalize('') != '':
+    raise Exception('Функция работает неверно!')
+
+print('Все тесты пройдены!')
